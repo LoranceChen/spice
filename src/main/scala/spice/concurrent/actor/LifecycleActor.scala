a1a886465bd8790a0f2757c5d85393f171202bfa
LoranceChen <wangchen@qiaobutang.com>
2016-01-17 22:15:36 +0800

    add method note

--- src/main/scala/spice/concurrent/actor/LifecycleActor.scala
+++ src/main/scala/spice/concurrent/actor/LifecycleActor.scala
@@ -3,52 +3,55 @@ package spice.concurrent.actor
 import akka.actor.{Props, ActorRef, Actor}
 
 /**
- *
- */
+  * preStart invoked in begin and after preRestart(on Exception), used to some logical operation.
+  * newTest executed in new object operation, for simple operation
+  */
 class LifecycleActor extends Actor with Logger {
   var child: ActorRef = _
+  val newTest = log.info("allocation - construct in begin")
+
   def receive = {
-    case num: Double => log.info(s"got a Double - $num")
-    case num: Int => log.info(s"got a Int - $num")
-    case lst: List[_] => log.info(s"got a List - ${lst.head}, ...")
+    case num: Double => log.info(s"receive - got a Double - $num")
+    case num: Int => log.info(s"receive - got a Int - $num")
+    case lst: List[_] => log.info(s"reveive - got a List - ${lst.head}, ...")
     case txt: String => child ! txt
   }
 
   override def preStart(): Unit = {
-    log.info("about to start")
+    log.info("preStart - about to start")
 
-    child = context.actorOf(Props[StringPrinter], "kiddo")
     //child actor does NOT create in current thread
-0  }
+    child = context.actorOf(Props[StringPrinter], "kiddo")
+  }
 
   override def preRestart(t: Throwable, msg: Option[Any]): Unit = {
-    log.info(s"about to restart because of $t, during message $msg")
+    log.info(s"preRestart - about to restart because of $t, during message $msg")
     super.preRestart(t, msg)
   }
 
   override def postRestart(t: Throwable): Unit = {
-    log.info(s"just restarted due to $t")
+    log.info(s"postRestart - just restarted due to $t")
     super.postRestart(t)
   }
 
-  override def postStop() = log.info("just stopped")
   //all child will stop on the follow
+  override def postStop() = log.info("postStop - just stopped")
 }
 
 class StringPrinter extends Actor with Logger {
   def receive = {
-    case msg: String => log.info(s"printer got message $msg")
+    case msg: String => log.info(s"receive - printer got message $msg")
   }
   override def preStart(): Unit = {
-    log.info(s"printer preStart")
+    log.info(s"preStart - printer preStart")
   }
 
   //parent exception does't affect child
   override def postRestart(t: Throwable): Unit = {
-    log.info(s"printer postRestart due to $t")
+    log.info(s"postRestart - printer postRestart due to $t")
   }
 
   override def postStop(): Unit = {
-    log.info(s"printer postStop")
+    log.info(s"postStop - printer postStop")
   }
 }
