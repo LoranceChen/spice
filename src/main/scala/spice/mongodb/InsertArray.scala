3c9d5bfa0fb2a2220a60e3f2a7929ba8a7cf5bf2
LoranceChen <wangchen@qiaobutang.com>
2016-07-04 10:37:06 +0800

    fix Array struct

--- src/main/scala/spice/mongodb/InsertArray.scala
+++ src/main/scala/spice/mongodb/InsertArray.scala
@@ -23,3 +23,63 @@ object InsertArray extends App {
 
   Thread.currentThread().join()
 }
+
+
+object InsertArray2 extends App {
+  val mongoClient: MongoClient = MongoClient()
+
+  val database: MongoDatabase = mongoClient.getDatabase("test")
+
+  val collection = database.getCollection("test_index2")
+
+  //result:(NOT expect)
+  /**
+    * {
+        "_id" : ObjectId("577647c281097f530523247e"),
+        "name" : "name_1",
+        "array" : [
+            [ //NOT expect
+                {
+                    "a" : "a_1",
+                    "b" : "b_1"
+                }
+            ],
+            [
+                {
+                    "a" : "a_2",
+                    "b" : "b_2"
+                }
+            ],...
+        ]
+      }
+    */
+  val documentsArrayOlder =  "array" -> (-10 to -1 map(x =>  List(Document("a" -> s"a_$x", "b" -> s"b_$x"))))
+
+  //long time needs
+  println("begin time - " + System.currentTimeMillis() / 1000)
+  val documentsArray =  "array" -> (-10 to -1 map(x =>  Document("a" -> s"a_$x", "b" -> s"b_$x")))
+  println("end time - " + System.currentTimeMillis() / 1000)
+
+  println("==================")
+
+  println("begin time - " + System.currentTimeMillis() / 1000)
+  val documents = 1 to 50 map(x => Document(documentsArray) ++ Document("name" -> s"name_$x") )
+  println("end time - " + System.currentTimeMillis() / 1000)
+
+  println("ready insert ...")
+  val insertObv = collection.insertMany(documents)
+  println("ready insert completed")
+  insertObv.subscribe(
+    (x: Completed) => {
+      println("result - " + x)
+    },
+    (e: Throwable) => {
+      println("throwable - " + e)
+    },
+    () => {
+      println("completed - ")
+    }
+  )
+
+  Thread.currentThread().join()
+}
