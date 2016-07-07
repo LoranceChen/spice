13ce2aea12d6856a18fddfc0c5ba62d5726a59be
LoranceChen <wangchen@qiaobutang.com>
2016-07-07 21:40:09 +0800

    another eg.

--- src/main/scala/spice/mongodb/InsertArray.scala
+++ src/main/scala/spice/mongodb/InsertArray.scala
@@ -1,5 +1,6 @@
 package spice.mongodb
 
+import org.bson.types.ObjectId
 import org.mongodb.scala.bson.BsonArray
 import org.mongodb.scala.bson.collection.immutable.Document
 import org.mongodb.scala.{Completed, MongoDatabase, MongoClient}
@@ -8,11 +9,11 @@ import org.mongodb.scala.{Completed, MongoDatabase, MongoClient}
   * find(_id -> .., array.item01 -> ...)
   */
 object InsertArray extends App {
-  val mongoClient: MongoClient = MongoClient()
+  val mongoClient: MongoClient = MongoClient("10.1.1.222")
 
   val database: MongoDatabase = mongoClient.getDatabase("test")
 
-  val collection = database.getCollection("test_index2")
+  val collection = database.getCollection("hr_invitations")
 
   val documents =  1 to 100000 map(x => Document("name" -> s"name_$x", "array" -> List("a" -> s"a_$x", "b" -> s"b_$x")))//.++(Document())))
   val insertObv = collection.insertMany(documents)
@@ -26,44 +27,46 @@ object InsertArray extends App {
 
 
 object InsertArray2 extends App {
-  val mongoClient: MongoClient = MongoClient()
+  val mongoClient: MongoClient = MongoClient("mongodb://10.1.1.222")
 
-  val database: MongoDatabase = mongoClient.getDatabase("test")
+  val database: MongoDatabase = mongoClient.getDatabase("resume")
 
-  val collection = database.getCollection("test_index2")
+  val collection = database.getCollection("hr_invitations")
 
   //result:(NOT expect)
   /**
     * {
-        "_id" : ObjectId("577647c281097f530523247e"),
-        "name" : "name_1",
-        "array" : [
-            [ //NOT expect
-                {
-                    "a" : "a_1",
-                    "b" : "b_1"
-                }
-            ],
-            [
-                {
-                    "a" : "a_2",
-                    "b" : "b_2"
-                }
-            ],...
-        ]
-      }
+    *"_id" : ObjectId("577647c281097f530523247e"),
+    *"name" : "name_1",
+    *"array" : [
+    *[ //NOT expect
+    *{
+    *"a" : "a_1",
+    *"b" : "b_1"
+    *}
+    *],
+    *[
+    *{
+    *"a" : "a_2",
+    *"b" : "b_2"
+    *}
+    *],...
+    *]
+    *}
     */
-  val documentsArrayOlder =  "array" -> (-10 to -1 map(x =>  List(Document("a" -> s"a_$x", "b" -> s"b_$x"))))
+//  val documentsArrayOlder =  "array" -> (-10 to -1 map(x =>  List(Document("a" -> s"a_$x", "b" -> s"b_$x"))))
 
   //long time needs
   println("begin time - " + System.currentTimeMillis() / 1000)
-  val documentsArray =  "array" -> (-10 to -1 map(x =>  Document("a" -> s"a_$x", "b" -> s"b_$x")))
+  val documentsArray =  "invites" -> (0 to 100000 map(x =>  Document("career_id" -> new ObjectId(), "job_id" -> x.toLong)))
+
+//  val documentsArray2 =  "invites" -> (0 to 100000 map(x =>  Document("career_id" -> new ObjectId(), "job_id" -> x.toLong)))
   println("end time - " + System.currentTimeMillis() / 1000)
 
   println("==================")
 
   println("begin time - " + System.currentTimeMillis() / 1000)
-  val documents = 1 to 50 map(x => Document(documentsArray) ++ Document("name" -> s"name_$x") )
+  val documents = 1 to 10 map(x => Document(documentsArray))
   println("end time - " + System.currentTimeMillis() / 1000)
 
   println("ready insert ...")
